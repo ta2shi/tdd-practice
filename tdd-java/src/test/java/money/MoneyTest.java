90b2eae832a57128c82866d91e25e72065323c51
Ta2shi <unison.tanii.0320@icloud.com>
2026-03-07 22:46:03 +0900

    feat(money): chapter-5 Francクラスの作成

--- tdd-java/src/test/java/money/MoneyTest.java
+++ tdd-java/src/test/java/money/MoneyTest.java
@@ -10,6 +10,13 @@ public class MoneyTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
+    
+    @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
 
     @Test
     public void testEquality() {
