7bee600fccd33951c86e89b6598e92fa83e98574
Ta2shi <unison.tanii.0320@icloud.com>
2026-03-07 22:53:27 +0900

    feat(money): chapter-6 親クラスとして、'Money'クラスの作成

--- tdd-java/src/test/java/money/MoneyTest.java
+++ tdd-java/src/test/java/money/MoneyTest.java
@@ -10,7 +10,7 @@ public class MoneyTest {
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
-    
+
     @Test
     public void testFrancMultiplication() {
         Franc five = new Franc(5);
@@ -22,5 +22,7 @@ public class MoneyTest {
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
+        assertTrue(new Franc(5).equals(new Franc(5)));
+        assertFalse(new Franc(5).equals(new Franc(6)));
     }
 }
